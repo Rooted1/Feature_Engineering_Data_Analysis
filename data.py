ea6b435cb6087272f4b98f51c3111473bb467977
Rooted1 <ruthobey@gmail.com>
2023-06-17 20:01:15 -0500

    print first seven data from dataframe and stats

--- data.py
+++ data.py
@@ -5,6 +5,9 @@ data = pd.read_csv('CalMod_06222009.csv')
 data = data.dropna()
 data = data.reset_index()
 
+# print first seven bands in DataFrame using list comprehension
+print(data[["Band{}".format(x) for x in range(1, 8)]])
+
 features = data.iloc[:, 2:44]
 
 # compute mean, min, max, std, var for all bands
@@ -19,6 +22,9 @@ stats = pd.concat([stats, min])
 stats = pd.concat([stats, max])
 stats = pd.concat([stats, std])
 stats = pd.concat([stats, var])
+
+# print first seven bands in stats using .loc[]
+print(stats.loc[:, "Band1" : "Band7"])
 
 # find band that has the worst spread using numpy array
 features_stats = stats.to_numpy()
